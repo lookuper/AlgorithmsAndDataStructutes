97d94cb857a29e0f4f932094c83602b729d367f0
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-08-14 23:20:39 +0300

    add HuffmanTree implementation

--- AlgoAndDS/Host/DS/Huffman.cs
+++ AlgoAndDS/Host/DS/Huffman.cs
@@ -62,22 +62,93 @@ namespace Host.DS
 
         public void Build(string source)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+
+            foreach (KeyValuePair<char, int> symbol in Frequencies)
+            {
+                nodes.Add(new HuffmanNode() { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                var orderedNodes = nodes.OrderBy(node => node.Frequency).ToList();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    // Take first two items
+                    var taken = orderedNodes.Take(2).ToList();
+
+                    // Create a parent node by combining the frequencies
+                    var parent = new HuffmanNode()
+                    {
+                        Symbol = '*',
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        Left = taken[0],
+                        Right = taken[1]
+                    };
+
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes.FirstOrDefault();
+            }
         }
 
         public BitArray Encode(string source)
         {
-            throw new NotImplementedException();
+            var encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                var encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+
+            BitArray bits = new BitArray(encodedSource.ToArray());
+            return bits;
         }
 
         public string Decode(BitArray bits)
         {
-            throw new NotImplementedException();
+            var current = this.Root;
+            string decoded = "";
+
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                        current = current.Right;
+                }
+                else
+                {
+                    if (current.Left != null)
+                        current = current.Left;
+                }
+
+                if (IsLeaf(current))
+                {
+                    decoded += current.Symbol;
+                    current = this.Root;
+                }                
+            }
+
+            return decoded;
         }
 
         public bool IsLeaf(HuffmanNode node)
         {
-            throw new NotImplementedException();
+            return (node.Left == null && node.Right == null) ;
         }
     }
 }
