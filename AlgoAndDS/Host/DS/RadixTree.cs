80b1b2f3a205ea3da04e69a6a650b56f2de51c9d
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-09-25 23:35:48 +0300

    firt part of implementation RadixTree

---
+++ AlgoAndDS/Host/DS/RadixTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host.DS
+{
+    public class RadixTree
+    {
+        public class Node
+        {
+            public String Label { get; set; }
+            public List<Node> SubNodes { get; set; }
+
+            public Node()
+            {
+                Label = String.Empty;
+                SubNodes = new List<Node>();
+            }
+
+            public Node(string l)
+            {
+                Label = l;
+                SubNodes = new List<Node>();
+            }
+        }
+
+        private Node root;
+
+        public RadixTree()
+        {
+            root = new Node();
+        }
+
+        public void Insert(string word)
+        {
+            InsertRec(word, root);
+        }
+
+        private void InsertRec(string wordPart, Node curNode)
+        {
+            var matches = MatchingConsecutiveCharacters(wordPart, curNode);
+
+            if ((matches == 0) || curNode == root || (matches > 0 && matches< wordPart.Length) && (matches >= curNode.Label.Length))
+            {
+                bool inserted = false;
+                var newWordPart = wordPart.Substring(matches, wordPart.Length - matches);
+
+                foreach (var  child in curNode.SubNodes)
+                {
+                    if (child.Label.StartsWith(newWordPart[0].ToString()))
+                    {
+                        inserted = true;
+                        InsertRec(newWordPart, child);
+                    }
+                }
+
+                if (inserted == false)
+                {
+                    curNode.SubNodes.Add(new Node(newWordPart));
+                }
+            } else if (matches < wordPart.Length)
+            {
+                string commonRoot = wordPart.Substring(0, matches);
+                string branchPreviousLabel = curNode.Label.Substring(matches, curNode.Label.Length - matches);
+                string branchNewLabel = wordPart.Substring(matches, wordPart.Length - matches);
+
+                curNode.Label = commonRoot;
+
+                var newNodePreviousLabel = new Node(branchPreviousLabel);
+                newNodePreviousLabel.SubNodes.AddRange(curNode.SubNodes);
+
+                curNode.SubNodes.Clear();
+                curNode.SubNodes.Add(newNodePreviousLabel);
+            } else if (matches == curNode.Label.Length)
+            {
+
+            } else if (matches > curNode.Label.Length)
+            {
+                var newNodeLabel = curNode.Label.Substring(curNode.Label.Length, wordPart.Length);
+                var newNode = new Node(newNodeLabel);
+                curNode.SubNodes.Add(newNode);
+            }           
+        }
+
+        private int MatchingConsecutiveCharacters(string word, Node curNode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
